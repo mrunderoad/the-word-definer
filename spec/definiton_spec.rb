c99b45fd9b310d00f2a81d1b50a9a2027efadd27
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-12-17 10:39:05 -0800

    first 4 definition tests passing

--- spec/definiton_spec.rb
+++ spec/definiton_spec.rb
@@ -1,2 +1,50 @@
 require 'rspec'
 require 'definition'
+
+describe '#Definition' do
+
+  before(:each) do
+    Word.clear()
+    Definition.clear()
+    @word = Word.new("Happy", nil)
+    @word.save()
+  end
+
+  describe('#save') do
+    it("saves a definition") do
+      definition = Definition.new("Cool Cat", @word.id, nil)
+      definition.save
+      expect(Definition.all).to(eq([definition]))
+    end
+  end  
+
+  describe('.clear') do
+    it("clears all songs") do
+      definition = Definition.new("Dog", @word.id, nil)
+      definition.save
+      definition2 = Definition.new("Cat", @word.id, nil)
+      definition2.save
+      Definition.clear
+      expect(Definition.all).to(eq([]))
+    end
+  end
+
+  describe('#==') do
+    it("is the same definition if the same as another") do
+      definition = Definition.new("cat", @word.id, nil)
+      definition2 = Definition.new("cat", @word.id, nil)
+      expect(definition).to(eq(definition2))
+    end
+  end
+
+  describe('.all') do
+    it("returns list of all definitions") do
+      definition = Definition.new("thats cool", @word.id, nil)
+      definition.save
+      definition2 = Definition.new("thats rad", @word.id, nil)
+      definition2.save
+      expect(Definition.all).to(eq([definition, definition2]))
+    end 
+  end
+
+end
