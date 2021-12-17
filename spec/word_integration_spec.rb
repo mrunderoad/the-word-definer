931d49e84cc6e4f95f2e114eedf36c216caace40
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-12-17 13:06:37 -0800

    second capybara test passed

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -12,3 +12,14 @@ describe('create a word path', {:type => :feature}) do
     expect(page).to have_content('Yellow')
   end
 end
+
+describe('create a definition path', {:type => :feature}) do
+  it('creates a word and then goes to the word page') do
+    word = Word.new("Yellow", nil)
+    word.save
+    visit("/words/#{word.id}")
+    fill_in('definition_name', :with => 'its a color')
+    click_on('Add definition')
+    expect(page).to have_content('its a color')
+  end
+end
